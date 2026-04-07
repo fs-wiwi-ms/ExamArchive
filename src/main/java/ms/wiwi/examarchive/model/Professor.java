562dbe812a8cfe40a32b07597ad692d8895410e8
ProfessorSam <76458774+ProfessorSam@users.noreply.github.com>
2026-04-07 15:55:17 +0200

    Hide unknown professors

--- src/main/java/ms/wiwi/examarchive/model/Professor.java
+++ src/main/java/ms/wiwi/examarchive/model/Professor.java
@@ -2,6 +2,9 @@ package ms.wiwi.examarchive.model;
 
 public record Professor(String professorID, String firstName, String lastName) {
     public String getFullName() {
+        if(firstName.equals("Unbekannt")){
+            return lastName;
+        }
         return firstName + " " + lastName;
     }
 }
